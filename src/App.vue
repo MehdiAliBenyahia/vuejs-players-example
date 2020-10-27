b80ffb1ad77be8a0e0a505c92ea5b62fecf59fad
ftoumi <ftoumi@gmail.com>
2020-10-27 12:25:20 +0100

    example project v1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Selection : {{ selection.length }}</div>
+  <player-list 
+    v-for="(player, index) in team.players"
+    :key="index"
+    :player="player"
+    @add-player="addPlayerToSelection"
+    >
+    </player-list>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import teamData from './data/players.json'
+import PlayerList from './components/PlayerList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PlayerList
+  },
+  data() {
+    return {
+      team: teamData,
+      selection: []
+    }
+  },
+  methods: {
+    addPlayerToSelection(player){
+      this.selection.push(player)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ 
 </style>
---
+++ src/components/PlayerList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="border-black border m-2 p-2 flex justify-between">
+    <div>
+      <h1 class="text-lg font-bold">{{ playerName }}</h1>
+      <p> {{ playerPositionUp }} </p>
+    </div>
+    <base-button
+      text="Sélectionner"
+      @click="addToSelection"
+    >
+    </base-button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "player-list",
+  props: ['player'],
+  computed: {
+    playerName(){
+      console.log(this.player.name.split('.'))
+      if(this.player.name.split('.').length > 1){
+        return this.player.name.split('.')[1]
+      }
+      return this.player.name;
+    },
+    playerPositionUp(){
+      return this.player.position.toUpperCase()
+    }
+  },
+  methods: {
+    addToSelection() {
+      this.$emit('add-player', this.player)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
