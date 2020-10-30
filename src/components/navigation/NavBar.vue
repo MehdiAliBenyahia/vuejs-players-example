d60fa39884fb1f5c1781fb82ca4128dc2ee239af
ALI BENYAHIA Mehdi <mehdi.ali_benyahia@edu.devinci.fr>
2020-10-30 17:09:43 +0100

    bouton

---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-blue-300 p-2">
+    <a href="#" @click="changeComponent('cour-list')">Tous les cours</a> |
+    <a href="#" @click="changeComponent('selection-list')">Ma sélection</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "nav-bar",
+  methods: {
+    changeComponent(value) {
+      this.$emit("change-component", value);
+    },
+  },
+};
+</script>
+
+<style></style>
